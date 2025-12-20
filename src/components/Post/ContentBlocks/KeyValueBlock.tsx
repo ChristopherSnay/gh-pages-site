872e4f408835fbb9757d79b86761450ca76c2445
Christopher Snay <christopher.snay@gmail.com>
2025-12-19 22:56:10 -0500

    Adds some quick draft resume items.

--- src/components/Post/ContentBlocks/KeyValueBlock.tsx
+++ src/components/Post/ContentBlocks/KeyValueBlock.tsx
@@ -9,7 +9,7 @@ export default function KeyValueBlock(props: Readonly<KeyValueBlockProps>) {
           key={index}
           className="d-flex flex-column mb-3 border-start border-3 border-warning ps-2 ms-2"
         >
-          <Typography variant="overline" fontWeight={800} className="lh-1">
+          <Typography variant="overline" fontWeight={700} className="lh-1">
             {item.key}
           </Typography>
           <Typography variant="body1" className="mt-1">
