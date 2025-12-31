2d7bf08d117274cedca909ed27b5a65a5f9c2611
Christopher Snay <christopher.snay@gmail.com>
2025-12-31 12:40:03 -0500

    Removes the image zoom dialog and replaces it with simple new-tab.

--- src/components/Post/ContentBlocks/ImageContentBlock.tsx
+++ src/components/Post/ContentBlocks/ImageContentBlock.tsx
@@ -1,48 +1,43 @@
 import { Box } from '@mui/material';
-import { useState } from 'react';
 import useImages from '../../../hooks/useImages';
 import type { ImageContentBlockData } from '../../../models/ImageContentBlockData';
-import ImageZoomDialog from '../../ImageZoomDialog';
 
 export default function ImageContentBlock(props: Readonly<ImageContentBlockProps>) {
-  const [isZoomDialogOpen, setIsZoomDialogOpen] = useState(false);
   const { localImage } = useImages();
 
+  const handleImageClick = (): void => {
+    window.open(localImage(props.data.url));
+  };
+
   return (
-    <>
-      <ImageZoomDialog
-        imageDetails={props.data}
-        isOpen={isZoomDialogOpen}
-        onClose={() => setIsZoomDialogOpen(false)}
-      />
-      <Box
-        component="img"
-        src={localImage(props.data.url)}
-        alt={props.data.alt}
-        className="py-4"
-        sx={{
-          borderRadius: 2,
-          display: 'block',
-          objectFit: 'contain',
-          width: '100%',
-          maxWidth: {
-            xs: '100%',
-            sm: '100%',
-            md: 600,
-            lg: 500
-          },
-          maxHeight: {
-            xs: '100%',
-            sm: '100%',
-            md: 400,
-            lg: 400
-          },
-          margin: 'auto',
-          boxShadow: 'inherit'
-        }}
-        onClick={() => setIsZoomDialogOpen(true)}
-      />
-    </>
+    <Box
+      component="img"
+      src={localImage(props.data.url)}
+      alt={props.data.alt}
+      className="py-4"
+      sx={{
+        borderRadius: 2,
+        cursor: 'pointer',
+        display: 'block',
+        objectFit: 'contain',
+        width: '100%',
+        maxWidth: {
+          xs: '100%',
+          sm: '100%',
+          md: 600,
+          lg: 500
+        },
+        maxHeight: {
+          xs: '100%',
+          sm: '100%',
+          md: 400,
+          lg: 400
+        },
+        margin: 'auto',
+        boxShadow: 'inherit'
+      }}
+      onClick={handleImageClick}
+    />
   );
 }
 
